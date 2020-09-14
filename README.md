ba2617052859d75c080c2da75cc4a9042c0df1d4
diego4x <diego444x@gmail.com>
2020-09-14 09:58:20 -0300

    funcao de texto e download criados

--- README.md
+++ README.md
@@ -9,9 +9,9 @@ Aplicação web para criar um meme em javascript
 
 [x] Pre visualizaçao da imagem 
 
-[] Adicionar texto a imagem
+[x] Adicionar texto a imagem
 
-[] fazer download da imagem com o texto
+[x] fazer download da imagem com o texto
 
 
 # Feito com ❤️ por [Diego Francisco](https://github.com/Diego4x)
--- main.js
+++ main.js
@@ -1,24 +1,77 @@
 const inputf = document.getElementById('inpf')
 
-const img = document.getElementById('img')
+const imgP = document.getElementById('img');
+
+const exp = document.getElementById('exp')
+
+const img = new Image();
+
+const inpt = document.getElementById('inpm')
+
+const inptb = document.getElementById('inptb')
 
 const span = document.getElementById('span')
-inputf.addEventListener("change", function(){
-   const file = this.files[0]
-   if(file){
-     const reader = new FileReader()
-     
-     img.style.display = 'block'
-     span.style.display = 'none'
-     
-    reader.addEventListener("load", function(){
-       img.setAttribute("src", this.result)
-     })
-     
-     reader.readAsDataURL(file)
-   }
-   else{
-     img.style.display = null
-     span.style.display = null
-   }
+
+let photoName;
+
+inputf.addEventListener("change", function() {
+  const file = this.files[0]
+  if (file) {
+    const reader = new FileReader()
+    photoName = file.name
+    imgP.style.display = 'block'
+    span.style.display = 'none'
+
+    reader.addEventListener("load", function() {
+      img.setAttribute("src", this.result)
+    })
+    reader.readAsDataURL(file)
+  }
+  else {
+    imgP.style.display = null
+    span.style.display = null
+  }
 })
+
+//canvas 
+
+let canvas = document.createElement('canvas')
+let ctx = canvas.getContext('2d')
+
+img.onload = function() {
+  const { width, height } = img;
+
+  canvas.width = width;
+  canvas.height = height;
+
+  ctx.clearRect(0, 0, width, height)
+
+  ctx.drawImage(img, 0, 0);
+  
+  textTop()
+  textBottom()
+
+  imgP.src = canvas.toDataURL()
+}
+
+
+function textTop() {
+  ctx.textAlign = 'center'
+  ctx.font = "50px Arial";
+  ctx.fillStyle = 'black'
+  ctx.fillText(inpt.value , canvas.width / 2, 50);
+}
+
+function textBottom(){
+  ctx.textAlign = 'center'
+  ctx.font = "50px Arial";
+  ctx.fillStyle = 'black'
+  ctx.fillText(inptb.value , canvas.width / 2, canvas.height - 20);
+}
+
+exp.onclick = function(){
+  const a = document.createElement('a')
+  a.download = photoName
+  a.href = canvas.toDataURL()
+  a.click()
+}
